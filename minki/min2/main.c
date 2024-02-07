bce3a4fe0f2c468fd18860d3c2d433a112ece7e4
IvannZ <zhukov.ivan05@yandex.ru>
2024-02-07 15:32:39 +0700

    make fully work min2

--- minki/min2/main.c
+++ minki/min2/main.c
@@ -10,10 +10,10 @@ struct halves_of_num {
 
 int counter = 0;
 
-int len_of_num(int num) {
+int len_of_num(unsigned long num) {
     //первая степень двойки которая больше чем длина числа
     unsigned long long pivot = 1;
-    unsigned long len = 1;
+    int len = 1;
     while (num > pivot) {
         pivot = pivot + (pivot << len);
         len = len << 1;
@@ -30,7 +30,6 @@ struct halves_of_num num_separator(unsigned long a, unsigned long len) {
 
 
 unsigned long _multiply(unsigned long a, unsigned long b, int max_len) {
-    //printf("a\n");
     if (max_len <= 3) {
         return a * b;
     }
@@ -38,41 +37,14 @@ unsigned long _multiply(unsigned long a, unsigned long b, int max_len) {
     struct halves_of_num halfs_b = num_separator(b, max_len);
     unsigned long first_step = _multiply(halfs_a.first_half, halfs_b.first_half, max_len >> 1);
     unsigned long second_step = _multiply(halfs_a.second_half, halfs_b.second_half, max_len >> 1);
-    int len_for_third_step =
-            max_len >> (1 - ((((halfs_a.first_half + halfs_a.second_half) & (1 << max_len)) == (1 << max_len)) ||
-                             (((halfs_b.first_half + halfs_b.second_half) & (1 << max_len)) == (1 << max_len))));
-    if (len_for_third_step != max(len_of_num(halfs_a.first_half + halfs_a.second_half), len_of_num(halfs_b.first_half + halfs_b.second_half))){
-   //     printf("her`s BUG\n");
-    }
-    if (max_len == len_for_third_step) {
-        counter++;
-        if (counter == 100) {
-            counter = 0;
-            return 0;
-        }
-        printf("counter:%d len:%d, a:%lu, fa:%lu, sa:%lu, b:%lu, fb:%lu, sb:%lu\n", counter, max_len, a,
-               halfs_a.first_half, halfs_a.second_half, b, halfs_b.first_half, halfs_b.second_half);
-    }
-    len_for_third_step = max_len>>1;
-    /*
-     что делает строка выше, она смотрит не перескочила ли длина суммы первой половины числа и второй половины числа в следующую 2^n пример
-     сумма составных числа 11111111(число в двоичной системе счисления) равняется 1111 + 1111 = 11110, и его длина равняется 5, а если судить в
-     2^n тогда длина равна 8, и мы должны его перемножать с учётом этого
-     либо же число такой же длины 10000000, и его сумма равняется 1000 + 0000 = 1000, что уже не 8, а 4 и его можно перемножать с длиной 4
-    */
-
-    // я не придумал как укоротить это условие, а переменную вынес для того чтобы было проще читать, можно сразу занести по применению
-
     unsigned long third_step = _multiply(halfs_a.first_half + halfs_a.second_half,
-                                         halfs_b.first_half + halfs_b.second_half,
-                                         len_for_third_step);
+                                         halfs_b.first_half + halfs_b.second_half, max_len >> 1);
     unsigned long fourth_step = third_step - second_step - first_step;
     return ((first_step << max_len) + second_step + (fourth_step << (max_len >> 1)));
 }
 
 unsigned long multiply(unsigned long a, unsigned long b) {
     counter = 0;
-    //printf("%d c mlt\n", a * b);
     return _multiply(a, b, max(len_of_num(a), len_of_num(b)));
 }
 
@@ -82,14 +54,14 @@ int main() {
 //не работает на отрицательных(можно сделать, но не вижу смысла)
     unsigned long a, b;
     printf("Enter -1 for start auto test, or\nEnter first num:");
-    scanf("%ld", &a);
+    scanf("%lu", &a);
     if (a == -1) {
         auto_test();
         return 0;
     }
     printf("Enter second num:");
-    scanf("%ld", &b);
-    printf("The answer is: %d\n", multiply(a, b));
+    scanf("%lu", &b);
+    printf("The answer is: %lu\n", multiply(a, b));
 
     return 0;
 }
@@ -123,6 +95,14 @@ void auto_test() {
         printf("%d * %d isn`t correct\n", a, b);
     }
 
+
+    a = 123456789, b = 123456789;
+    if (a * b == multiply(a, b)) {
+        printf("%d * %d is correct\n", a, b);
+    } else {
+        printf("%d * %d isn`t correct\n", a, b);
+    }
+
     a = 545, b = 0;
     if (a * b == multiply(a, b)) {
         printf("%d * %d is correct\n", a, b);
@@ -135,15 +115,15 @@ void auto_test() {
             printf("%d * %d = %lu is correct\n", i - 1, i - 1, multiply(i - 1, i - 1));
         } else {
             printf("%d * %d = %lu isn`t correct\n", i - 1, i - 1,
-                   multiply(i - 1, i - 1));// пишет это потому-что с перемножает в int
+                   multiply(i - 1, i - 1));// пишет это потому-что си перемножает в int
         }
         i *= 2;
     }
-    /*
-    a = (1 << (sizeof(int) * 8)-1)-1, b = 1;
+    a = (1 << (sizeof(int) * 8) - 1) - 1, b = 1;
     if (a * b == multiply(a, b)) {
         printf("%d * %d is correct\n", a, b);
     } else {
         printf("%d * %d isn`t correct\n", a, b);
-    }*/
+    }
+
 }
