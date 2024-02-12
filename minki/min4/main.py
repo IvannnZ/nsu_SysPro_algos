c5695bb890d1556faafac11aecb528d2331bfe4d
IvannZ <zhukov.ivan05@yandex.ru>
2024-02-12 19:49:30 +0700

    one more time full make min 4

--- minki/min4/main.py
+++ minki/min4/main.py
@@ -3,47 +3,39 @@ from subprocess import check_output
 from subprocess import run
 from sys import argv
 
-#_, git_path, first_git_commit ,last_git_commit ,program_for_check = argv
+_, git_path, first_git_commit, last_git_commit, program_for_check = argv
+
+# git_path = "dir_for_test"
+# first_git_commit = "6c5d97c"
+# last_git_commit = "b1906a8"
+# program_for_check = "./ultraprogram"
 
 
-git_path = ".."
+os.chdir(git_path)
 
-first_git_commit = ""
-last_git_commit = ""
-
-program_for_check = ("")
-
-args = {'rep_path': git_path, 'first_commit': first_git_commit, 'latest_commit': last_git_commit,
-        'command': program_for_check}
-
-os.chdir(args['rep_path'])
-
-os.system('git log --oneline')
-
-git_log_command = f"git log --oneline"  # TODO сюда добавить диапазон
-git_log = check_output(git_log_command, shell=True).decode().splitlines()
+git_log = check_output(f"git log {first_git_commit}..{last_git_commit} --oneline", shell=True).decode().splitlines()
 commits = list(i.split()[0] for i in git_log)
-commits.append(args['first_commit'])
-
-print(commits)
+commits.append(first_git_commit)
+print('Commits:', commits)
 
 
 def check_commit(commit):
     os.system(f'git checkout {commit}')
-    return run(args['command']) == 0
+    answ = run(program_for_check, capture_output=True).returncode == 0
+    os.system('git checkout -')
+    return answ
 
 
 count_for_check = len(commits)
-
 
 left_edge = 0
 right_edge = len(commits)
 
 while right_edge - left_edge > 1:
-    pivot = (right_edge + left_edge)/2
+    pivot = int((right_edge + left_edge) / 2)
     if check_commit(commits[pivot]):
         right_edge = pivot
     else:
         left_edge = pivot
 
-print(left_edge)
+print(commits[left_edge])
