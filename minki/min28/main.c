e16457b9c95b992195775d8ffc9b71f85448e7e4
IvannZ <zhukov.ivan05@yandex.ru>
2024-05-07 22:04:43 +0700

    start debug

--- minki/min28/main.c
+++ minki/min28/main.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include "stdio.h"
-#include ""
+#include "math.h"
+//компилятору нужно добавить флаг -lm
+
+#define big_simple_num 1306717
 
 void is_null(void *ptr) {
     if (ptr == NULL) {
@@ -8,7 +11,67 @@ void is_null(void *ptr) {
     }
 }
 
+struct blum_filter {
+    size_t size;
+    size_t number_hash;
+    unsigned int *arr_hash_seed;
+    unsigned int *data;
+};
 
+unsigned int get_bit(const unsigned int *arr_ptr, unsigned int bit_num) {
+    unsigned int place_in_arr = bit_num / sizeof(unsigned int);
+    unsigned int place_in_num = bit_num % sizeof(unsigned int);
+    printf("%d, %d\n", (1 << place_in_num), place_in_num);
+    return arr_ptr[place_in_arr] && (1 << place_in_num);
+}
+
+void set_bit(unsigned int *arr_ptr, unsigned int bit_num) {
+    unsigned int place_in_arr = bit_num / sizeof(unsigned int);
+    unsigned int place_in_num = bit_num % sizeof(unsigned int);
+    printf("%d, %d\n", (1 << place_in_num), place_in_num);
+    arr_ptr[place_in_arr] = arr_ptr[place_in_arr] || (1 << place_in_num);
+}
+
+unsigned int hash(unsigned int value, unsigned int seed) {
+    return (unsigned int) ((value * seed) % big_simple_num);
+}
+
+struct blum_filter create_blum_filter(size_t number_request, float error_probability) {
+    struct blum_filter bf;
+    size_t bit_for_elem = (size_t) (-log2f(error_probability) / log(2));
+    bf.size = bit_for_elem * number_request;
+    bf.data = (unsigned int *) malloc(
+            sizeof(unsigned int) * (size_t) ceilf(ceilf((float) bf.size / 8) / (float) sizeof(unsigned int)));
+    is_null(bf.data);
+    printf("data size:%lu\n",
+           sizeof(unsigned int) * (size_t) ceilf(ceilf((float) bf.size / 8) / (float) sizeof(unsigned int)));
+    bf.number_hash = (size_t) (log(2) * (float) bit_for_elem);
+    bf.arr_hash_seed = (unsigned int *) malloc(sizeof(unsigned int) * bf.number_hash);
+    is_null(bf.arr_hash_seed);
+    for (int i = 0; i < bf.number_hash; i++) {
+        bf.arr_hash_seed[i] = rand();
+    }
+    return bf;
+}
+
+void add_to_blum_filter(struct blum_filter bf, unsigned int value) {
+    printf("start add value:%d\n", value);
+    for (int i = 0; i < bf.number_hash; i++) {
+        set_bit(bf.data, hash(value, bf.arr_hash_seed[i]) % bf.size);
+        printf("hash:%zu, seed:%zu\n", hash(value, bf.arr_hash_seed[i]) % bf.size, bf.arr_hash_seed[i]);
+    }
+    printf("end %d,%d,%d,%d\n\n", bf.data[0], bf.data[1], bf.data[2], bf.data[3]);
+}
+
+int check_to_blum_filter(struct blum_filter bf, unsigned int value) {
+
+    for (int i = 0; i < bf.number_hash; i++) {
+        if (get_bit(bf.data, hash(value, bf.arr_hash_seed[i]) % bf.size) == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
 
 
 int main(int argc, char *argv[]) {
@@ -18,10 +81,19 @@ int main(int argc, char *argv[]) {
     printf("\n\n%s\n\n", argv[1]);
     FILE *input = fopen(argv[1], "r");
     is_null(input);
-    int number_request;
+    size_t number_request;
     float error_probability;
-    fscanf(input, "%d %f", &number_request, &error_probability);
-    printf("%d %f", number_request, error_probability);
-    int
-
+    fscanf(input, "%zd %f", &number_request, &error_probability);
+    struct blum_filter bf = create_blum_filter(number_request, error_probability);
+    unsigned int ip_adr;
+    while (fscanf(input, "%d", &ip_adr) != EOF) {
+        add_to_blum_filter(bf, ip_adr);
+    }
+    for (unsigned int i = 0; i < number_request; i++) {
+        printf("%d in bitset: %d\n", i * 2, check_to_blum_filter(bf, i * 2));
+    }
 }
+//    printf("%d, %d, %d\n", hash(9, 564894), hash(9, 564894), hash(9, 564894));
+//    printf("%d, %d, %d\n", hash(52, 564894), hash(52, 564894), hash(52, 564894));
+//    printf("%d, %d, %d\n", hash(42, 564894), hash(42, 564894), hash(42, 564894));
+//    printf("%d, %d, %d\n", hash(10, 564894), hash(10, 564894), hash(10, 564894));
